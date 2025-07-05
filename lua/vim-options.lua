76d8c445a51c6e96a8982e59bbc6634f5f627201
Lucas Martini <lucas.leite.martini@gmail.com>
2025-07-05 16:53:29 -0400

    keymap file added

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,18 +6,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
-vim.opt.mouse = "a"
-vim.opt.clipboard = "unnamedplus"
-
-vim.keymap.set("n", "<leader>nb", ":bnext<CR>")
-vim.keymap.set("n", "<leader>pb", ":bprev<CR>")
-vim.keymap.set("n", "<leader>d", ":bd<CR>")
-vim.keymap.set("n", "<leader>ns", ":nohlsearch<CR>")
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
-vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-vim.keymap.set("n", "gd", vim.lsp.buf.declaration, {})
-vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-vim.keymap.set({ "n", "v" }, "<leader>i", vim.lsp.buf.format, {})
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
